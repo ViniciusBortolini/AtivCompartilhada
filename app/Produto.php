2f4a023cdf2589dfb4a93d200ae73eedd7638ee4
S0LUTI0N <Lukraus1212@hotmail.com>
2020-05-03 22:48:22 -0300

    Relacionamento N:N Venda e Produto

--- app/Produto.php
+++ app/Produto.php
@@ -8,4 +8,10 @@ class Produto extends Model
 {
     protected $primaryKey ='id';
     protected $table = 'produtos';
+
+    function vendas() {
+    	return $this->belongsToMany('App\Venda', 'produtos_vendas', 'id_produto', 'id_venda')
+    	->withPivot(['quantidade', 'subtotal'])
+    	->withTimestamps();
+    }
 }
